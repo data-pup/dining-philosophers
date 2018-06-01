42169a5c03d4c2bb172bf022f49dd9a9fa698c61
data-pup <kevinmartinucr@gmail.com>
2018-06-01 11:58:34 -0400

    Added a basic example of threading.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::thread;
+
+// struct Spoon {
+//     counter: u32,
+// }
+
+// impl Spoon {
+// }
+
+// struct Philosopher {
+// }
+
+// impl Philosopher {
+// }
+
+fn create_thread() -> thread::JoinHandle<()> {
+    let v = vec![1, 2, 3];
+    thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    let handle = create_thread();
+    handle.join().expect("Could not join");
 }
